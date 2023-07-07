9d1680607689aea6aff16b79bb23ebed0ad5d10d
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-07 16:51:44 -0400

    verified tests work, and refactored a few parameters in functions to make more sense and me more abstract

--- tests/model_builder_tests.py
+++ tests/model_builder_tests.py
@@ -1,13 +1,20 @@
 import unittest
-from src.model_builder import construct_model
+import sys
+import json
+sys.path.append('src')
+from model_builder import construct_model
 
 class ConstructModelTesting(unittest.TestCase):
 
-    def test_construct(self):
+    def test_construct(self): #Verify correct via json print
         test_act_0 = 'relu'
         test_act_1 = 'selu'
         test_classes = 2
         test_optimizer = 'adamax'
         test_model = construct_model.construct(dense_activation_0=test_act_0,dense_activation_1=test_act_1,num_classes=test_classes,optimizer=test_optimizer)
 
-        
+        with open('tests/model_json_dump.json','w') as jf:
+            jf.writelines(json.dumps(test_model.get_config(),indent=4))
+
+if __name__ == '__main__':
+    unittest.main()
