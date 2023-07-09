0d267ec2cc41cca83b7d668b1485750851a35a0d
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-09 15:51:42 -0400

    tested and finalized making a dataset from a directory, and added test images for said purpose

--- src/model_builder/construct_dataset.py
+++ src/model_builder/construct_dataset.py
@@ -1,6 +1,8 @@
 from tensorflow import keras
-from construct_model import get_labels, extract_classes
+import tensorflow as tf
+from model_builder.construct_model import get_labels, extract_classes
 from ReissLib.PickyPixels import image_verification as iv
+from matplotlib import pyplot as plt
 
 def get_data(link, classes_file, batch_size, val_split=None, csvfile=None):
     '''
@@ -23,15 +25,29 @@ def get_data(link, classes_file, batch_size, val_split=None, csvfile=None):
                                                labels=labels,
                                                label_mode='int', #we want integer encoding
                                                class_names=extract_classes(classes_file),
-                                               color_mode='grayscale', #standardizes the data first
+                                               color_mode='grayscale', #ensures all images are grayscale, as few arent
                                                batch_size=batch_size,
                                                image_size=(224,224,3),
                                                shuffle=(True if labels else False), #if we are training, we want to shuffle
                                                seed= 19121954, #arbitrarily chosen. RIP Alan Turing
                                                validation_split=val_split, #split only if we are training
                                                subset='both')
-        dataset = preprocess(dataset) #we aren't done preprocessing the data.
+        dataset = preprocess(dataset) #Need to convert all images to RGB for vgg16 weights. Also normalizes data
         return dataset
     except: #TODO: include dropbox link handling
         print('ERROR: NON-DIRECTORY PROVIDED. THE MODEL DOES NOT CURRENTLY SUPPORT DROPBOX LINKS')
         return 0
+
+def preprocess(dataset:tf.data.Dataset|list[tf.data.Dataset]):
+    '''
+    Takes in a Dataset object, normalizes the images, and changes the color mode to RGB.
+    Returns: Modified Dataset object
+    '''
+    if(type(dataset) == list): #we passed in both a training and validation subset, so process both
+        dataset = [preprocess(dataset[0]), preprocess(dataset[1])]
+        return dataset if (dataset[0] and dataset[1]) is not None else 0
+    try:
+        dataset = dataset.map(lambda x: tf.image.grayscale_to_rgb(x/255)) #divide by 255 to normalize, then rgb the images
+        return dataset
+    except:
+        return None
--- tests/construct_dataset_tests.py
+++ tests/construct_dataset_tests.py
@@ -3,14 +3,35 @@ import sys
 import json
 import os
 sys.path.append('src')
-from model_builder import construct_dataset
+from model_builder import construct_dataset, construct_model
 from tensorflow import keras
+import tensorflow as tf
 
 class ConstructDatasetTesting(unittest.TestCase):
     def test_get_data_non_valid_link(self):
         self.assertFalse(construct_dataset.get_data('apples',None,None))
     
-    def test_get_data_valid_directory_training(self):
+    '''def test_get_data_valid_directory_training(self):
         directory = 'tests/testing_data/'
-        self.assertIs()
+        datset = construct_dataset.get_data()
+        self.assertIs()'''
+
+    def test_preprocess_empty(self):
+        self.assertFalse(construct_dataset.preprocess(None))
+    
+    def test_preprocess_not_dataset(self):
+        self.assertFalse(construct_dataset.preprocess(1))
+    
+    def test_preprocess_valid_dataset(self):
+        ds = keras.utils.image_dataset_from_directory('tests\\testing_data', labels=None, batch_size=8, color_mode='grayscale')
+        self.assertTrue(construct_dataset.preprocess(ds))
+    
+    def test_preprocess_both_subsets(self):
+        ds = keras.utils.image_dataset_from_directory('tests\\testing_data', labels=None, batch_size=8, seed=19121954, validation_split=0.3, color_mode='grayscale', subset='both')
+        self.assertTrue(construct_dataset.preprocess(ds))
+    
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
+
     
