97399fc2bc2f1c76b864b4f6ecaaf8dcd22e0399
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-11 17:15:27 -0400

    refactored code to include more try/except statements for debugging, added logging code to construct_model, improved documentation

--- src/model_builder/construct_model.py
+++ src/model_builder/construct_model.py
@@ -6,6 +6,7 @@ import json
 import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
+import logging
 
 def construct(dense_activation_0, dense_activation_1, optimizer, num_classes):
     '''
@@ -15,30 +16,39 @@ def construct(dense_activation_0, dense_activation_1, optimizer, num_classes):
     num_classes: int; represents the number of classes available
     Return: keras.Sequential model instance
     '''
-
-    VGG = VGG16(input_shape = (224, 224, 3), 
-                include_top = False,
-                weights = 'imagenet', 
-                classes = num_classes)
-
-    VGG.trainable = False #we dont want to train the first 13 layers, just the last 3.
-
-    #model is flattened to feed into final dense layers
-    #in final dense layer, the number of units must match how many possible classifications there are
-    #softmax converts all results into a probability of it being that classification (val btw. 0-1)
-    model = Sequential([VGG,                  
-                        Flatten(),
-                        Dense(units = 1024, activation = dense_activation_0),
-                        Dense(units = 1024, activation = dense_activation_1),
-                        Dense(units = num_classes, activation = 'softmax')])
-
-    #compile the model
-    #sparse_categorical_crossentropy: because we have a non-binary classification + want integer encoding, not 1-hot and we are encoding the predictions as integers
-    model.compile(optimizer = optimizer,                       
-                loss = 'sparse_categorical_crossentropy', 
-                metrics = ['accuracy']) #uses MSE
+    logging.info('construct() called.')
     
-    return model
+    logging.debug('Attempting to make VGG model')
+    try:
+        VGG = VGG16(input_shape = (224, 224, 3), 
+                    include_top = False,
+                    weights = 'imagenet', 
+                    classes = num_classes)
+        logging.info('Weights assigned.')
+
+        VGG.trainable = False #we dont want to train the first 13 layers, just the last 3.
+        logging.info('VGG.trainable: False')
+
+        #model is flattened to feed into final dense layers
+        #in final dense layer, the number of units must match how many possible classifications there are
+        #softmax converts all results into a probability of it being that classification (val btw. 0-1)
+        model = Sequential([VGG,                  
+                            Flatten(),
+                            Dense(units = 1024, activation = dense_activation_0),
+                            Dense(units = 1024, activation = dense_activation_1),
+                            Dense(units = num_classes, activation = 'softmax')])
+        logging.info('Last three layers added successfully.')
+        #compile the model
+        #sparse_categorical_crossentropy: because we have a non-binary classification + want integer encoding, not 1-hot and we are encoding the predictions as integers
+        model.compile(optimizer = optimizer,                       
+                    loss = 'sparse_categorical_crossentropy', 
+                    metrics = ['accuracy']) #uses MSE
+        logging.info('Model compiled.')
+        
+        return model
+    except:
+        logging.exception('Unknown Error: Could not construct model.')
+        return 0
 
 def extract_classes(location):
     '''
@@ -46,10 +56,13 @@ def extract_classes(location):
     location: filepath + name of file we are opening
     Returns: dict instance | 0
     ''' 
+    logging.info('extract_classes() called')
     try:
+        logging.debug('Trying to read classes file')
         with open(location, 'r') as classes:
             return json.load(classes)
     except:
+        logging.debug('Classes file not found. Returning 0')
         return 0
 
 
@@ -60,14 +73,18 @@ def get_new_classes(csvfile, current_classes):
     current_classes: dict of currently registered classes. Is None if they do not exist
     returns: dict instance
     '''
+    logging.info('get_new_classes() called')
     classes = {} #empty dict to use
     #if we have pre-existing classes, set classes to point to them
     if(current_classes is not None):
+        logging.debug('current_classes is not empty... prepping to add to it if necessary.')
         classes = current_classes
-    
+    logging.debug(f'classes: {"not empty" if classes else "empty"}')
     next_entry = len(classes) #len indexes starting at 1, not 0. Thus, next_entry = last value + 1
+    logging.debug(f'class dict next_entry:{next_entry}')
     for label in get_labels(csvfile):
         if classes.get(label) is None: #if the class doesnt exist
+            logging.debug(f'found new class. it is: {label}')
             classes[label] = next_entry #add it to the dictionary with the corresponding int encoding
             next_entry += 1
     
@@ -79,15 +96,21 @@ def save_classes(classes, location):
     classes: dict object of classes
     location: file path + file name to save the json file at. Will also be appended with '.OLD' to backup old file
     '''
+    logging.info('save_classes() called.')
     if(path.exists(f'{location}.OLD')): #checks to see if we have an old
+        logging.debug('classes OLD file found. attempting to remove it')
         remove(f'{location}.OLD') #if so, remove it for the next one
     try:
+        logging.debug('Trying to rename old class file as OLD')
         rename(location, f'{location}.OLD') #if json.old existed, its gone now.
-    except: #Thus, the only error that can arise is that there is no current class file
+    except:
+        logging.exception('OldFileNotFoundError. This should not be an issue unless it exists')
         pass #we dont need to do anything
     finally: #always save
+        logging.debug('Attempting to save new classes file. Warning: will overwrite if it exists but was not found in revious block.')
         with open(location, 'w') as fp:
             json.dump(obj=classes, fp=fp)
+            logging.debug('file created.')
 
 def save_model(model:tf.keras.models.Model, location:str):
     '''
@@ -95,15 +118,19 @@ def save_model(model:tf.keras.models.Model, location:str):
     model: tf.keras.models.Model instance
     location: file path + file name to save the model file at. Will also be appended with '.OLD' to backup old file
     '''
+    logging.info('save_model() called.')
     if(path.exists(f'{location}.OLD')): #checks to see if we have an old
+        logging.debug('model OLD file found. attempting to remove it')
         remove(f'{location}.OLD') #if so, remove it for the next one
     try:
+        logging.debug('Trying to rename old model file as OLD')
         rename(location, f'{location}.OLD') #if .old existed, its gone now.
-    except: #Thus, the only error that can arise is that there is no current class file
+    except: 
+        logging.exception('OldFileNotFoundError. This should not be an issue unless it exists')
         pass #we dont need to do anything
     finally: #always save
+        logging.debug('Attempting to save new model file. Warning: will overwrite if it exists but was not found in revious block.')
         model.save(f'{location}')
-    pass
 
 def get_labels(csvfile): #TODO: #10 add exception handling for if csvfile isnt a csv here!
    '''
@@ -111,11 +138,17 @@ def get_labels(csvfile): #TODO: #10 add exception handling for if csvfile isnt a
    csvfile: filepath to csv file with images and labels
    Returns: list of labels (duplicates included)
    '''
+   logging.info('get_labels() called.')
    try: #csvfile might be None
+    logging.debug('Trying to extract data from a csv file')
     csvfile =  pd.read_csv(csvfile)
-    new_classes = csvfile.loc[:,'Animal'].to_list()
-    return new_classes
+    logging.debug('Success.')
+    logging.debug('Trying to extract labels')
+    labels = csvfile.loc[:,'Animal'].to_list()
+    logging.debug('Success.')
+    return labels
    except:
+       logging.exception('Unknown exception thrown. Labels were not able to be extracted')
        return None
 
 def load_model(model_name, classes_file):
@@ -123,29 +156,50 @@ def load_model(model_name, classes_file):
     Loads the model and its classes as a dict.
     Returns: tuple(keras.models.Model, dict{classes})
     '''
+    logging.info('load_model() called.')
     return (models.load_model(model_name), extract_classes(classes_file))
 
 def train_model(model:models.Model, dataset:tf.data.Dataset, steps_per_epoch:int|None, epochs:int, batch_size:int, validation_steps:int|None):
-    es1 = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience = 3) #stops training the network if validation loss doesn't improve after 3 epochs
-    hist = model.fit(x = dataset[0],                        #the training data
-                     steps_per_epoch = steps_per_epoch,     #None defaults to the number of batches
-                     epochs = epochs,                       #performed best in experimentation
-                     batch_size=batch_size,                 #memory allocation issues
-                     callbacks = es1,                       #Tells the model to monitor es1; in this case, monitor val_loss with patience of 3
-                     validation_data = dataset[1],          #the validation data
-                     validation_steps = validation_steps,   #None defaults to the number of batches
-                     verbose = 1)                           #1 shows progress bar. Helps gauge how much is done
+    '''
+    Trains a model on a given dataset with specified hyperparameters.
+    model: a tf.keras.models.Model
+    dataset: a list[tf.data.Dataset] of training and validation data
+    steps_per_epoch: int referring to how many gradient updates per epoch to do
+    epochs: int; represents the number of epochs to run the model for
+    batch_size: int; representing the number of images per batch
+    validation_steps: int; represents how many backpropagations per run on the validation data. For best performance, this should be None.
     
-    save_model()
-    return hist
+    Returns: History object
+    '''
+    logging.info('train_model() called.')
+    es1 = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience = 3) #stops training the network if validation loss doesn't improve after 3 epochs
+    logging.debug(f'Early stopping set. es1:{es1}')
+    try:
+        logging.debug('Trying to train the model. Running fit()...')
+        hist = model.fit(x = dataset[0],                        #the training data
+                        steps_per_epoch = steps_per_epoch,     #None defaults to the number of batches
+                        epochs = epochs,                       #performed best in experimentation
+                        batch_size=batch_size,                 #memory allocation issues
+                        callbacks = es1,                       #Tells the model to monitor es1; in this case, monitor val_loss with patience of 3
+                        validation_data = dataset[1],          #the validation data
+                        validation_steps = validation_steps,   #None defaults to the number of batches
+                        verbose = 1)                           #1 shows progress bar. Helps gauge how much is done
+        
+        save_model()
+        return hist
+    except:
+        logging.exception('CRITICAL!!! MODEL WAS UNABLE TO BE TRAINED: UNKNOWN EXCPETION: RETURNING 0')
+        return 0
 
 def visualize_performance(hist):
     '''
     Creates charts displaying the model's accuracy and loss metrics.
     hist: the result of running model.fit()
     '''
+    logging.info('visualize_model() called.')
+    logging.info('Saving model accuracy graph')
     #The following code creates a graph of the accuracy of the model
-    plt.title('AVFC Model Accuracy')
+    plt.title('AVFC Model Accuracy (fMSE)')
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy')
     plt.plot(hist.history['accuracy'])
@@ -153,6 +207,7 @@ def visualize_performance(hist):
     plt.legend(['Accuracy', 'Validation Accuracy'])
     plt.show()
 
+    logging.info('Saving model loss graph (fCE)')
     #The following code creates a graph of the loss of the model
     plt.title('AVFC Model Loss')
     plt.xlabel('Epoch')
