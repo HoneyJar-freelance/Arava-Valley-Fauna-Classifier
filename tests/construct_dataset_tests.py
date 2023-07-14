6a46b6b99ef91b1d023558ef6c83e896e437ae72
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-14 19:27:25 -0400

    wrote more tests to ensure integrity of program; have not run them

--- tests/construct_dataset_tests.py
+++ tests/construct_dataset_tests.py
@@ -21,10 +21,20 @@ class ConstructDatasetTesting(unittest.TestCase):
         ds = keras.utils.image_dataset_from_directory('tests\\testing_data', labels=None, batch_size=8, color_mode='grayscale')
         self.assertTrue(construct_dataset.preprocess(ds))
     
+    def test_preprocess_valid_dataset_verify_color_change(self):
+        ds = keras.utils.image_dataset_from_directory('tests\\testing_data', labels=None, batch_size=8, color_mode='grayscale', image_size=(224,224))
+        self.assertEqual((None, 224, 224, 3), tf.shape(ds))
+    
     def test_preprocess_both_subsets(self):
         ds = keras.utils.image_dataset_from_directory('tests\\testing_data', labels=None, batch_size=8, seed=19121954, validation_split=0.3, color_mode='grayscale', subset='both')
         self.assertTrue(construct_dataset.preprocess(ds))
+
+    def test_get_data_valid_info(self):
+        ds = construct_dataset.get_data(link='tests\\testing_data', batch_size=32,val_split=None,csvfile='tests\\testing_data\\testing_labels.csv')
+        self.assertTrue(ds)
     
+    def test_get_data_batch_none(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
