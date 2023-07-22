2a642e0c44254075633b803057287c5282e4fefd
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-22 17:29:24 -0400

    added stubs for dataset creation + helper functions

---
+++ src/model_builder/construct_dataset_new.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow import keras
+from construct_model import get_labels, extract_classes
+import logging
+
+def create_dataset():
+    pass
+
+def preprocess():
+    pass
+
+def rgb_and_normalize():
+    pass
+
+def one_hot_encode_labels():
+    pass
