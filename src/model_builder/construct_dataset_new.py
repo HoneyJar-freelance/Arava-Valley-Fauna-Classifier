dc786a4e3e05c5c4b810fc0f0c409d56d83b193c
HoneyJar-0 <76642194+HoneyJar-0@users.noreply.github.com>
2023-07-30 10:51:01 -0400

    finished prepare_datasets with documentation

--- src/model_builder/construct_dataset_new.py
+++ src/model_builder/construct_dataset_new.py
@@ -47,10 +47,15 @@ def create_dataset(path:str, csvfile:str, classes:dict, batch_size:int, val_spli
     if(len(file_paths) == 0):
         raise ValueError('No valid files were found for dataset creation')
     
-    if(labels is None and (val_split is None or val_split == 0)):
+    if(not bool(labels) and not bool(val_split)):
         raise ValueError('Both csvfile and val_split must be specified if either is specified.'
                          f'csvfile file given: {csvfile}'
                          f'val_split given: {val_split}')
+    
+    if(len(labels) != len(file_paths)):
+        raise ValueError('Length mismatch on labels and file_paths:'
+                         f'#labels: {len(labels)}'
+                         f'#files : {len(file_paths)}')
 
     path_ds = tf.data.Dataset.from_tensor_slices(file_paths) #dataset of file_paths
     path_ds = path_ds.flat_map(lambda file: load_image(file), num_parallel_calls=tf.data.AUTOTUNE) #allows images to be loaded on runtime, and applies relevant transformations
@@ -63,22 +68,55 @@ def create_dataset(path:str, csvfile:str, classes:dict, batch_size:int, val_spli
     train_ds, val_ds = prepare_datasets(path_ds, label_ds, val_split, batch_size)
 
 def prepare_datasets(path_ds:tf.data.Dataset, label_ds:tf.data.Dataset, val_split:float, batch_size:int) -> list[tf.data.Dataset]:
+    '''
+    Creates a dataset(s) used by the program for image classification.
+
+    Args:
+        path_ds: tf.data.Dataset that contains a list of file paths with mapped transformations
+        labels_ds: tf.data.Dataset that contains a list of labels corresponding to path_ds
+        val_split: The validation split (range of [0, 1)) of the data. != 0 if training, = 0 else
+        batch_size: The number of images per batch. Excess is not dropped
     
+        Rules:
+            - len(path_ds) == len(labels_ds)
+            - 0 <= val_split < 1
+            - batch_size > 0
+    
+    Returns:
+        list[tf.data.Dataset] if val_split is non-zero, else a singular tf.data.Dataset. The dataset is combined from path_ds and labels_ds.
+    '''
+    #TODO: rename function for clarity
     #handle exceptions
     if(not path_ds):
         raise ValueError('You must specify a dataset object for path_ds. None/Invalid given.')
-    if(not batch_size):
+    if(not batch_size or batch_size < 1):
         raise ValueError(f'Invalid value for batch_dataset. Should be int > 0, given {batch_size}')
     if(bool(label_ds) != bool(val_split)):
         raise ValueError('Invalid arguments for label_ds and val_split. Both must be present or absent.'
                          f'label_ds: {label_ds}     val_split: {val_split}')
     
     if(val_split): #Then we are training, thus shuffle data + split data
+        if(val_split < 0 or val_split >= 1):
+            raise ValueError(f'Invalid val_split given. Should be in range [0, 1). Given: {val_split}')
         train_imgs, train_labels = get_training_or_validation_split(path_ds, label_ds, val_split, 'training')
         val_imgs, val_labels = get_training_or_validation_split(path_ds, label_ds, val_split, 'validation')
 
+        #combine image datasets with their respective labels
+        train_ds = tf.data.Dataset.from_tensor_slices((train_imgs, train_labels))
+        val_ds = tf.data.Dataset.from_tensor_slices((val_imgs, val_labels))
+
+        #shuffle data
         seed = random(1e-6)
-        path_ds.shuffle(buffer_size= batch_size*8, seed=seed)
+        train_ds = train_ds.shuffle(buffer_size= batch_size*8, seed=seed)
+        
+        #batch datasets
+        train_ds = train_ds.batch(batch_size=batch_size)
+        val_ds = val_ds.batch(batch_size=batch_size)
+
+        return [train_ds, val_ds]
+    
+    else:
+        return [tf.data.Dataset.from_tensor_slices((path_ds, label_ds))]
 
 def load_image(path) -> tf.Tensor:
     '''
